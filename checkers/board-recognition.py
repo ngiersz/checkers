509fbd440bcdc7a13d7882255ca0e5c7a8aacda0
Natalia Gierszewska <ngiersz@gmail.com>
2019-04-01 10:48:11 +0200

    Cleanup

--- checkers/board-recognition.py
+++ checkers/board-recognition.py
@@ -1,6 +1,7 @@
 import cv2
 import os
 from enum import Enum
+import numpy as np
 
 
 def save_image(img, prefix):
@@ -24,29 +25,19 @@ class Field(Enum):
     BLACK_WITH_PLAYER_1 = 3
     BLACK_WITH_PLAYER_2 = 4
 
+
+board_shape = [8, 8]
+board = np.array(board_shape)
+
+
 if __name__ == '__main__':
-    print(cv2.__version__)
     pattern = cv2.imread('pattern.png', cv2.IMREAD_GRAYSCALE)
-    print(pattern.shape)
-    ret, left_corners = cv2.findChessboardCorners(pattern, (6, 9))
-    # print(left_corners)
-    # print(ret)
+    ret, left_corners = cv2.findChessboardCorners(pattern, (board_shape[0]-1, board_shape[1]-1))
     pattern_with_corners = pattern
     for corner in left_corners:
-        print(corner)
-        coord_a = 0
-        coord_b = 0
-        for x in corner:
-            first = True
-            for y in x:
-                if first:
-                    coord_a = y
-                    first = False
-                else:
-                    coord_b = y
-        # print('(' + str(coord_a) + ', ' + str(coord_b) + ')')
-        cv2.circle(pattern_with_corners, (coord_a, coord_b), 10, (255, 0, 0), -1)
-        # cv2.circle(img, (447, 63), 63, (0, 0, 255), -1)
+        coord = np.array([int(corner.item((0, 0))), int(corner.item((0, 1)))])
+        print(coord)
+        cv2.circle(pattern_with_corners, (coord[0], coord[1]), 10, (255, 0, 0), -1)
 
     save_image(pattern_with_corners, 'pattern_with_corners')
 
