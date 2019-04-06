8e28a56bb829a2a233a2714f20b1a6fd095947a8
lilatee <marcin.hradowicz@gmail.com>
2019-04-06 03:31:18 +0200

    working on detection of pawns

---
+++ checkers/Field.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class Field(Enum):
+    WHITE = 0
+    BLACK = 1
+    BLACK_FIELD_BLUE_PAWN = 2
+    BLACK_FIELD_RED_PAWN = 3
--- checkers/main.py
+++ checkers/main.py
@@ -2,33 +2,9 @@ import cv2
 import ShapeDetector
 import argparse
 import imutils
-import time
 import numpy as np
-from matplotlib import pyplot as plt
+from Field import Field
 import requests
-
-
-def show_webcam(cam, mirror=False):
-
-    while True:
-        ret_val, img = cam.read()
-
-        # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        # ret, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
-        if mirror:
-            img = cv2.flip(img, 1)
-
-        # img = searchChessboard(img)
-        # img = findCheesboard(img)
-        cv2.imshow('Chessboard', img)
-
-        if cv2.waitKey(1) == 27:
-            break  # esc to quit
-    cv2.destroyAllWindows()
-    # ret_val, img = cam.read()
-    # cv2.imshow("Asdasd",img)
-    # cv2.waitKey(0)
-    # return img
 
 
 def findFields(image):
@@ -66,73 +42,103 @@ def findFields(image):
     print(len(cnts))
     if len(cnts) != 64:
         return None
+    print("refreshed")
 
-    # cntsSorted = []
-    # for i in range(len(cnts)-1):
-    #     if i % 2 == 0:
-    #         cntsSorted.append(cnts[int(i/2)])
-    #     else:
-    #         cntsSorted.append(cnts[32 + int(i/2)])
+    cntsSorted = []
+    values = []
+    for i in range(len(cnts)-1):
+        if i % 2 == 0:
+            cntsSorted.append(cnts[int(i/2)])
+            values.append(Field.BLACK)
+        else:
+            cntsSorted.append(cnts[32 + int(i/2)])
+            values.append(Field.WHITE)
+
 
     sd = ShapeDetector.ShapeDetector()
-
-    # cntsSorted[0][1][0][1] = 99
-    # print(cntsSorted[0])
-    # print(cntsSorted[0][0][0][0])
-    # g = image[cntsSorted[0][0][0][1]:cntsSorted[0][2][0][1],cntsSorted[0][0][0][0]:cntsSorted[0][2][0][0]]
-    # cv2.imshow("pierwsze pole", g)
-    # loop over the contours
-    for c in cnts:
+    pointsOfFields = []
+    for c in cntsSorted:
         shape = sd.detect(c)
         if shape == 'square':
-            cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
-        # compute the center of the contour, then detect the name of the
-        # shape using only the contour
-        # M = cv2.moments(c)
-        # cX = int((M["m10"] / M["m00"]))
-        # cY = int((M["m01"] / M["m00"]))
-        # cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
-        #             0.5, (255, 255, 255), 2)
+            # cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+            x = 0
+            y = 0
+            count = 0
+            for t in c:
+                x = x + t[0][0]
+                y = y + t[0][1]
+                count = count + 1
+            x = int(x / count)
+            y = int(y / count)
+            pointsOfFields.append([x, y])
+    return pointsOfFields
 
 
+def findPawns(image, threshold):
+    imageBlack = image.copy()
+    imageWhite = image.copy()
 
-    return image
+    gray = cv2.cvtColor(imageBlack, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    thresh = cv2.threshold(blurred, threshold, 255, cv2.THRESH_BINARY)[1]
+    kernel = np.ones((5, 5), np.uint8)
+    cv2.imshow("pawnsThreshBeforeErode"+str(threshold), thresh)
+    thresh = cv2.erode(thresh, kernel, iterations=1)
+    cv2.imshow("pawnsThresh"+str(threshold), thresh)
+    # find contours in the thresholded imageBlack and initialize the
+    # shape detector
+    cnts1 = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)
+    # contures of black fields
+    cnts = imutils.grab_contours(cnts1)
+
+    cnts.reverse()
+    print(str(threshold),len(cnts))
+
+    pointsOfPawns = []
+    sd = ShapeDetector.ShapeDetector()
+    for c in cnts:
+        shape = sd.detect(c)
+        if shape == 'circle':
+            # cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+            x = 0
+            y = 0
+            count = 0
+            for t in c:
+                x = x + t[0][0]
+                y = y + t[0][1]
+                count = count + 1
+            x = int(x/count)
+            y = int(y/count)
+            pointsOfPawns.append([x, y])
+            # cv2.circle(image, (x, y), 15, (255, 0, 0), 2)
+    return pointsOfPawns
 
 
-def order_points(pts):
-    # initialzie a list of coordinates that will be ordered
-    # such that the first entry in the list is the top-left,
-    # the second entry is the top-right, the third is the
-    # bottom-right, and the fourth is the bottom-left
-    rect = np.zeros((4, 2), dtype="float32")
+def checkIfPawnOnField(fields, pawnsBlue, pawnsRed, image):
+    result = []
+    for i in range(64):
+        if i % 2 == 0:
+            result.append(Field.BLACK)
+        else:
+            result.append(Field.WHITE)
 
-    # the top-left point will have the smallest sum, whereas
-    # the bottom-right point will have the largest sum
-    s = pts.sum(axis=1)
-    rect[0] = pts[np.argmin(s)]
-    rect[2] = pts[np.argmax(s)]
+    for id, field in enumerate(fields):
+        for pawn in pawnsBlue:
+            if abs(field[0] - pawn[0]) <= 20 and abs(field[1] - pawn[1]) <= 20:
+                cv2.circle(image, (field[0], field[1]), 10, (255, 0, 0), 3)
+                result[id] = Field.BLACK_FIELD_BLUE_PAWN
+                print("blue")
+        for pawn in pawnsRed:
+            if abs(field[0] - pawn[0]) <= 20 and abs(field[1] - pawn[1]) <= 20:
+                cv2.circle(image, (field[0], field[1]), 10, (0, 255, 0), 3)
+                result[id] = Field.BLACK_FIELD_RED_PAWN
+                print("red")
 
-    # now, compute the difference between the points, the
-    # top-right point will have the smallest difference,
-    # whereas the bottom-left will have the largest difference
-    diff = np.diff(pts, axis=1)
-    rect[1] = pts[np.argmin(diff)]
-    rect[3] = pts[np.argmax(diff)]
-
-    # return the ordered coordinates
-    return rect
+    cv2.imshow("hehhe", image)
+    print(result)
 
 def main():
-    # cam = cv2.VideoCapture(1)
-    # cam.set(3, 640)
-    # cam.set(4, 480)
-    # cam.set(5, 75)
-    # show_webcam(cam)
-
-    # image = cv2.imread("chessboardARUCO.png", 0)
-    # cv2.imshow("dsfsd", image)
-    # cv2.waitKey(0)
-
     url = "http://192.168.1.66:8080/shot.jpg"
     while True:
         img_resp = requests.get(url)
@@ -140,16 +146,29 @@ def main():
         img = cv2.imdecode(img_arr, -1)
 
         try:
+            img = cv2.imread("images/chessboardARUCO.png", 1)
             img = findChessboard(img)
-            img = findFields(img)
+            pawnsBlue = findPawns(img, 10)
+            pawnsRed = findPawns(img, 200)
+            fields = findFields(img)
+            checkIfPawnOnField(fields, pawnsBlue, pawnsRed, img)
+            cv2.waitKey(0)
             if img is None:
                 continue
-            cv2.imshow("Wykryte pola", img)
+            # cv2.imshow("Wykryte pola", img)
         except:
             continue
         if cv2.waitKey(1) == 27:
             break
 
+    # img2 = cv2.imread("images/chessboardARUCO_2.png", 1)
+    # img = findChessboard(img2)
+    # pawnsBlue = findPawns(img, 30)
+    # pawnsRed = findPawns(img, 60)
+    # fields = findFields(img)
+    # checkIfPawnOnField(fields, pawnsBlue, pawnsRed, img)
+    # cv2.imshow("Wykryte pola", img)
+    # cv2.waitKey(0)
 
 def findChessboard(image):
     ULx = None
@@ -161,16 +180,14 @@ def findChessboard(image):
     DRx = None
     DRy = None
 
-
-
-    markersNames = ('DOWN_LEFT.png', 'UP_RIGHT.png', 'UP_LEFT.png', 'DOWN_RIGHT.png')
+    markersNames = ('images/DOWN_LEFT.png', 'images/UP_RIGHT.png', 'images/UP_LEFT.png', 'images/DOWN_RIGHT.png')
 
     # id is one of the corners of detected image that we need
-    img2 = image  # trainImage
+    img2 = image.copy()  # trainImage
     for id, markerName in enumerate(markersNames):
         img1 = cv2.imread(markerName, 0)  # queryImage
 
-        MIN_MATCH_COUNT = 10
+        MIN_MATCH_COUNT = 5
 
         # Initiate SIFT detector
         sift = cv2.xfeatures2d.SIFT_create()
@@ -198,13 +215,9 @@ def findChessboard(image):
             dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
 
             M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
-            matchesMask = mask.ravel().tolist()
-
             h, w = img1.shape
             pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
             dst = cv2.perspectiveTransform(pts, M)
-
-            # img2 = cv2.polylines(img2, [np.int32(dst)], True, (255, 0, 0), 3, cv2.LINE_AA)
 
             if id == 0:
                 DRx = [np.int32(dst)[id]][0][0][0]
@@ -219,64 +232,14 @@ def findChessboard(image):
                 DLx = [np.int32(dst)[id]][0][0][0]
                 DLy = [np.int32(dst)[id]][0][0][1]
 
-        else:
-            # print("Not enough matches are found - %d/%d" % (len(good), MIN_MATCH_COUNT))
-            pass
-            matchesMask = None
-
-        # draw_params = dict(matchColor=(0, 255, 0),  # draw matches in green color
-        #                    singlePointColor=None,
-        #                    matchesMask=matchesMask,  # draw only inliers
-        #                    flags=2)
-        #
-        # img3 = cv2.drawMatches(img1, kp1, img2, kp2, good, None, **draw_params)
-
-    # result = image[URy:DRy, ULx:URx]
 
     pts1 = np.float32([[ULx, ULy], [URx, URy], [DLx, DLy], [DRx, DRy]])
     pts2 = np.float32([[0, 0], [500, 0], [0, 500], [500, 500]])
     M = cv2.getPerspectiveTransform(pts1, pts2)
     result = cv2.warpPerspective(image, M, (500, 500))
+    print(result.shape)
     return result
 
-
-def four_point_transform(image, pts):
-    # obtain a consistent order of the points and unpack them
-    # individually
-    rect = order_points(pts)
-    (tl, tr, br, bl) = rect
-
-    # compute the width of the new image, which will be the
-    # maximum distance between bottom-right and bottom-left
-    # x-coordiates or the top-right and top-left x-coordinates
-    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
-    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
-    maxWidth = max(int(widthA), int(widthB))
-
-    # compute the height of the new image, which will be the
-    # maximum distance between the top-right and bottom-right
-    # y-coordinates or the top-left and bottom-left y-coordinates
-    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
-    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
-    maxHeight = max(int(heightA), int(heightB))
-
-    # now that we have the dimensions of the new image, construct
-    # the set of destination points to obtain a "birds eye view",
-    # (i.e. top-down view) of the image, again specifying points
-    # in the top-left, top-right, bottom-right, and bottom-left
-    # order
-    dst = np.array([
-        [0, 0],
-        [maxWidth - 1, 0],
-        [maxWidth - 1, maxHeight - 1],
-        [0, maxHeight - 1]], dtype="float32")
-
-    # compute the perspective transform matrix and then apply it
-    M = cv2.getPerspectiveTransform(rect, dst)
-    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
-
-    # return the warped image
-    return warped
 
 if __name__ == '__main__':
         main()
